a396619902ad3ce6eac9af45c25b8ab55de649e3
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2025-08-22 21:38:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/villainmusic777/VILLAIN_MUSIC",
+    "https://github.com/istkharalam62/MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
